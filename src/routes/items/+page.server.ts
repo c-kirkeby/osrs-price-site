7270ac0daa7ade1546d1aa7538cf7c803c3c2616
c-kirkeby <christian@christiankirkeby.com>
2023-11-07 21:30:58 +1000

    feat: add cache headers

--- src/routes/items/+page.server.ts
+++ src/routes/items/+page.server.ts
@@ -1,7 +1,10 @@
 import { syncUpstreamPrices } from "$lib/db/sync";
 import { fetchItems } from "$lib/server/items";
 
-export async function load() {
+export async function load(params) {
+  params.setHeaders({
+    "Cache-Control": "max-age=60",
+  });
   await syncUpstreamPrices();
   return {
     items: fetchItems(),
--- src/routes/items/+server.ts
+++ src/routes/items/+server.ts
@@ -3,7 +3,10 @@ import { items } from "$lib/db/schema";
 import { json } from "@sveltejs/kit";
 import { syncUpstreamPrices } from "$lib/db/sync";
 
-export async function GET() {
+export async function GET(event) {
+  event.setHeaders({
+    "Cache-Control": "max-age=60",
+  });
   await syncUpstreamPrices();
   const itemQuery = db.select().from(items).execute();
   const data = await itemQuery;
