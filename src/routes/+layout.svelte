97c1f42dae6f14e021928843c98bf8d67d842ac9
c-kirkeby <christian@christiankirkeby.com>
2023-11-08 21:47:11 +1000

    feat: layout improvements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,53 @@
 <script>
   import { page } from "$app/stores";
-  import { Button } from "$lib/components/ui/button";
-  import { GaugeIcon, ListIcon } from "lucide-svelte";
+  import { MenuIcon, X, GaugeIcon, ListIcon } from "lucide-svelte";
   import { cn } from "$lib/utils";
   import "../app.postcss";
+
+  let menuItems = [
+    { href: "/", title: "Home", icon: GaugeIcon },
+    { href: "/items", title: "Items", icon: ListIcon },
+  ];
+
+  let showMobileMenu = false;
 </script>
 
-<div class="space-y-6 p-10 pb-16">
-  <div class="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-    <aside class="-mx-4 lg:w-1/12">
-      <nav class="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1">
-        {#each [{ href: "/", title: "Home", icon: GaugeIcon }, { href: "/items", title: "Items", icon: ListIcon }] as { href, title, icon }}
-          <Button
-            {href}
-            variant="ghost"
-            class={cn(
-              $page.url.pathname === href
-                ? "bg-muted hover:bg-muted"
-                : "hover:bg-transparent hover:underline",
-              "justify-start",
-            )}
-          >
-            <svelte:component this={icon} class="h-4 w-4 mr-2" />
-            {title}
-          </Button>
-        {/each}
-      </nav>
-    </aside>
-    <main class="flex-1">
-      <slot />
-    </main>
+<div class="min-h-screen bg-background font-sans antialiased">
+  <div class="flex min-h-screen flex-col space-y-6">
+    <header class="sticky sticky-top-0 z-40 border-b bg-background">
+      <div class="container flex h-16 items-center justify-between py-4">
+        <nav class="hidden gap-6 md:flex">
+          {#each menuItems as { href, title }}
+            <a
+              {href}
+              class={cn(
+                "flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm",
+                $page.url.pathname === href
+                  ? "text-foreground"
+                  : "text-foreground/60",
+              )}
+            >
+              {title}
+            </a>
+          {/each}
+        </nav>
+        <button
+          class="flex items-center space-x-2 md:hidden"
+          on:click={() => (showMobileMenu = !showMobileMenu)}
+        >
+          {#if showMobileMenu}
+            <X />
+          {:else}
+            <MenuIcon />
+          {/if}
+          <span class="font-bold">Menu</span>
+        </button>
+      </div>
+    </header>
+    <div class="container grid">
+      <main class="flex w-full flex-1 flex-col overflow-hidden">
+        <slot />
+      </main>
+    </div>
   </div>
 </div>
