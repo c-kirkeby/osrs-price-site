8e2a3858705c7ce02c8a275d545f5dca7739553a
c-kirkeby <christian@christiankirkeby.com>
2024-02-28 21:38:22 +1000

    feat: improve settings menu
    - Replace dropdown with popover
    - Save settings to localStorage

--- src/lib/components/settings-menu.svelte
+++ src/lib/components/settings-menu.svelte
@@ -1,25 +1,36 @@
 <script lang="ts">
   import { Button } from "$lib/components/ui/button";
-  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
-  import SettingsIcon from "lucide-svelte/icons/settings";
-
+  import { Switch } from "$lib/components/ui/switch";
+  import { Label } from "$lib/components/ui/label";
+  import * as Popover from "$lib/components/ui/popover";
   import { settings } from "$lib/stores/settings";
+  import { SettingsIcon } from "lucide-svelte";
 </script>
 
-<DropdownMenu.Root>
-  <DropdownMenu.Trigger asChild let:builder>
+<Popover.Root portal={null}>
+  <Popover.Trigger asChild let:builder>
     <Button builders={[builder]} variant="ghost" size="icon">
       <span class="sr-only">Settings</span>
       <SettingsIcon class="h-4 w-4" />
     </Button>
-  </DropdownMenu.Trigger>
-  <DropdownMenu.Content>
-    <DropdownMenu.CheckboxItem
-      bind:checked={$settings.compact}
-      onCheckedChange={() =>
-        settings.update((store) => ({ ...store, compact: !$settings.compact }))}
-    >
-      Compact Mode
-    </DropdownMenu.CheckboxItem>
-  </DropdownMenu.Content>
-</DropdownMenu.Root>
+  </Popover.Trigger>
+  <Popover.Content class="grid gap-6">
+    <div class="flex items-center justify-between space-x-2">
+      <Label for="necessary" class="flex flex-col space-y-1">
+        <span>Compact Mode</span>
+        <span class="text-xs font-normal leading-snug text-muted-foreground">
+          Turning this off will make the content take up the full width.
+        </span>
+      </Label>
+      <Switch
+        id="compact"
+        bind:checked={$settings.compact}
+        onCheckedChange={() =>
+          settings.update((store) => ({
+            ...store,
+            compact: !$settings.compact,
+          }))}
+      />
+    </div>
+  </Popover.Content>
+</Popover.Root>
--- src/lib/stores/settings.ts
+++ src/lib/stores/settings.ts
@@ -1,18 +1,28 @@
 import { writable } from "svelte/store";
+import { browser } from "$app/environment";
 
 export interface Settings {
   compact: boolean;
 }
 
 function createSettings(initialSettings: Settings) {
-  const { subscribe, update } = writable(initialSettings);
+  const store = writable(initialSettings, () => {
+    const subscribe = store.subscribe((value) => {
+      if (browser) {
+        localStorage.setItem("settings", JSON.stringify(value));
+      }
+    });
+    return subscribe;
+  });
 
-  return {
-    subscribe,
-    update,
-  };
+  return store;
 }
+
+const initialSettings = JSON.parse(
+  browser ? localStorage.getItem("settings") : "{}",
+);
 
 export const settings = createSettings({
   compact: true,
+  ...initialSettings,
 });
