e0817a62ccd364719dd0c731d1eff2c903a38260
c-kirkeby <christian@christiankirkeby.com>
2024-02-29 21:39:17 +1000

    feat: persist stores to local storage

--- src/lib/stores/searchHistory.ts
+++ src/lib/stores/searchHistory.ts
@@ -1,18 +1,28 @@
 import type { Item } from "$lib/db/schema";
-import { writable } from "svelte/store";
+import { writable, type Writable } from "svelte/store";
+import { browser } from "$app/environment";
 
 function createSearchHistory(initialItems: Partial<Item>[]) {
-  const { subscribe, update } = writable(initialItems);
+  const store: Writable<Partial<Item>[]> = writable(initialItems, () => {
+    const subscribe = store.subscribe((value) => {
+      if (browser) {
+        localStorage.setItem("history", JSON.stringify(value));
+      }
+    });
+    return subscribe;
+  });
+
+  const { subscribe, update } = store;
 
   function add(item: Partial<Item>) {
     update((items) => {
-      return [item, ...items.filter((i) => i.id !== item.id)].slice(0, 5);
+      return [item, ...items.filter((i) => i?.id !== item.id)].slice(0, 5);
     });
   }
 
   function remove(item: Partial<Item>) {
     update((items) => {
-      const newItems = items.filter((i) => i.id !== item.id);
+      const newItems = items.filter((i) => i?.id !== item.id);
       return newItems;
     });
   }
@@ -24,4 +34,8 @@ function createSearchHistory(initialItems: Partial<Item>[]) {
   };
 }
 
-export const searchHistory = createSearchHistory([]);
+const initialHistory = JSON.parse(
+  browser ? localStorage.getItem("history") ?? "[]" : "[]",
+);
+
+export const searchHistory = createSearchHistory(initialHistory);
--- src/lib/stores/settings.ts
+++ src/lib/stores/settings.ts
@@ -1,4 +1,4 @@
-import { writable } from "svelte/store";
+import { writable, type Writable } from "svelte/store";
 import { browser } from "$app/environment";
 
 export interface Settings {
@@ -6,7 +6,7 @@ export interface Settings {
 }
 
 function createSettings(initialSettings: Settings) {
-  const store = writable(initialSettings, () => {
+  const store: Writable<Settings> = writable(initialSettings, () => {
     const subscribe = store.subscribe((value) => {
       if (browser) {
         localStorage.setItem("settings", JSON.stringify(value));
@@ -19,8 +19,8 @@ function createSettings(initialSettings: Settings) {
 }
 
 const initialSettings = JSON.parse(
-  browser ? localStorage.getItem("settings") : "{}",
-);
+  browser ? localStorage.getItem("settings") ?? "{}" : "{}",
+) as Partial<Settings>;
 
 export const settings = createSettings({
   compact: true,
