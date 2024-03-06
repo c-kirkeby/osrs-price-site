cc7eb63408bc5bc89161a7c8ea7fa1a383a65894
c-kirkeby <christian@christiankirkeby.com>
2024-03-06 13:09:41 +1000

    feat: add layerchart

--- src/lib/components/charts/price-series-chart.svelte
+++ src/lib/components/charts/price-series-chart.svelte
@@ -2,17 +2,17 @@
   import type { TimeSeries } from "$lib/types/time-series";
   import { getCompactNumberFormatter, getNumberFormatter } from "$lib/utils";
   import {
-    VisAxis,
-    VisBulletLegend,
-    VisCrosshair,
-    VisLine,
-    VisScatter,
-    VisTooltip,
-    VisXYContainer,
-  } from "@unovis/svelte";
-  import { Line } from "@unovis/ts";
-  import { formatDistanceToNowStrict, formatRelative } from "date-fns";
-  import capitalize from "lodash/capitalize";
+    Chart,
+    Svg,
+    Axis,
+    Spline,
+    Tooltip,
+    TooltipItem,
+    Highlight,
+  } from "layerchart";
+  import { formatDistanceToNowStrict } from "date-fns";
+  import { scaleTime } from "d3-scale";
+  import { format } from "date-fns/format";
 
   $: numberFormatter = getNumberFormatter();
   $: compactNumberFormatter = getCompactNumberFormatter();
@@ -21,99 +21,90 @@
 
   $: avgHighPriceSeries = data.filter((d) => d.avgHighPrice !== null);
   $: avgLowPriceSeries = data.filter((d) => d.avgLowPrice !== null);
+  $: yDomain = [Math.min(...avgLowPriceSeries.map(avgLowPriceAccessor)), null];
 
-  const x = (data: TimeSeries) => data.timestamp;
-  const y = [
-    (data: TimeSeries) => (data.avgHighPrice ? data.avgHighPrice : undefined),
-    (data: TimeSeries) => (data.avgLowPrice ? data.avgLowPrice : undefined),
-  ];
+  function avgHighPriceAccessor(d: TimeSeries) {
+    return d.avgHighPrice;
+  }
 
-  const yAvgLowPrice = (data: TimeSeries) => data.avgLowPrice;
-  const yAvgHighPrice = (data: TimeSeries) => data.avgHighPrice;
+  function avgLowPriceAccessor(d: TimeSeries) {
+    return d.avgLowPrice;
+  }
 
-  const triggers = {
-    [Line.selectors.line]: (data: TimeSeries) => data.avgHighPrice,
-  };
-  const XTickFormat = (tick: number) =>
-    formatDistanceToNowStrict(new Date(tick * 1000), {
+  function formatTimestamp(d: TimeSeries["timestamp"]) {
+    return formatDistanceToNowStrict(new Date(d * 1000), {
       addSuffix: true,
     });
+  }
 
-  const YTickFormat = (tick: number) => compactNumberFormatter.format(tick);
-  const items = [
-    {
-      name: "Buy Price",
-    },
-    {
-      name: "Sell Price",
-    },
-  ];
-
-  const template = (data: TimeSeries) => {
-    return `
-      <div class="grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-background px-2.5 py text-xs shadow-xl">
-        <div class="grid gap-1.5">
-          <div class="text-xs col-span-2 text-foreground">
-            ${capitalize(formatRelative(data.timestamp * 1000, new Date()).slice())}
-          </div>
-          <div class="grid gap-1.5">
-            <div class="flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 [&>svg]:text-muted-foreground">
-              <div class="shrink-0 rounded-[2px] border-[var(--vis-color0)] bg-[var(--vis-color0)] w-2.5 h-2.5"></div>
-              <div class="flex flex-1 justify-between leading-none">
-                <span class="text-muted-foreground">Buy Price</span>
-              </div>
-              <span class="font-mono font-medium tabular-nums text-foreground">${numberFormatter.format(data.avgHighPrice)}</span>
-            </div>
-            <div class="flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 [&>svg]:text-muted-foreground">
-              <div class="shrink-0 rounded-[2px] border-[var(--vis-color1)] bg-[var(--vis-color1)] w-2.5 h-2.5"></div>
-              <div class="flex flex-1 justify-between leading-none">
-                <span class="text-muted-foreground">Sell Price</span>
-              </div>
-              <span class="font-mono font-medium tabular-nums text-foreground">${numberFormatter.format(data.avgLowPrice)}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
-  };
+  function formatPrice(
+    d: TimeSeries["avgHighPrice"] | TimeSeries["avgLowPrice"],
+  ) {
+    return compactNumberFormatter.format(d);
+  }
 </script>
 
 {#if data.length > 0}
-  <VisXYContainer
-    class="font-sans text-muted-foreground"
-    height="250"
-    padding={{
-      top: 5,
-      right: 10,
-      left: 10,
-      bottom: 0,
-    }}
-    --vis-tooltip-border-color="rgba(0, 0, 0, 0)"
-    --vis-tooltip-background-color="rgba(0, 0, 0, 0)"
-    --vis-dark-tooltip-border-color="rgba(0, 0, 0, 0)"
-    --vis-dark-tooltip-background-color="rgba(0, 0, 0, 0)"
-  >
-    <VisLine
-      curveType="linear"
-      data={avgLowPriceSeries}
-      {x}
-      y={yAvgLowPrice}
-      color="var(--vis-color1)"
-    />
-    <VisLine
-      curveType="linear"
-      data={avgHighPriceSeries}
-      {x}
-      y={yAvgHighPrice}
-      color="var(--vis-color0)"
-    />
-    <VisTooltip {triggers} />
-    <VisAxis type="x" tickFormat={XTickFormat} gridLine={false} />
-    <VisAxis type="y" tickFormat={YTickFormat} gridLine={false} />
-    <VisScatter {data} {x} {y} size={4} />
-    <VisBulletLegend {items} />
-    <VisCrosshair {data} {template} />
-  </VisXYContainer>
+  <div class="pb-4">
+    <div class="h-[200px]">
+      <Chart
+        {data}
+        x="timestamp"
+        y="avgHighPrice"
+        y1="avgLowPrice"
+        xScale={scaleTime()}
+        {yDomain}
+        yNice
+        padding={{
+          top: 5,
+          right: 10,
+          left: 10,
+          bottom: 0,
+        }}
+        tooltip={{
+          mode: "voronoi",
+        }}
+      >
+        <Svg>
+          <Axis
+            placement="left"
+            format={formatPrice}
+            rule={{ class: "stroke-primary" }}
+            labelProps={{
+              class: "fill-primary",
+            }}
+          />
+          <Axis
+            placement="bottom"
+            format={formatTimestamp}
+            rule={{ class: "stroke-primary" }}
+            labelProps={{
+              class: "fill-primary",
+            }}
+          />
+          <Spline
+            data={avgHighPriceSeries}
+            y={avgHighPriceAccessor}
+            class="stroke-2 stroke-primary"
+          />
+          <Spline
+            data={avgLowPriceSeries}
+            y={avgLowPriceAccessor}
+            class="stroke-2 stroke-secondary"
+          />
+          <Highlight points lines />
+        </Svg>
+        <Tooltip
+          header={(d) =>
+            format(new Date(d.timestamp * 1000), "yyyy-MM-dd HH:mm:ss")}
+          let:data
+        >
+          <TooltipItem label="High Price" value={data.avgHighPrice} />
+          <TooltipItem label="Low Price" value={data.avgLowPrice} />
+        </Tooltip>
+      </Chart>
+    </div>
+  </div>
 {:else}
   <div class="h-[250px] flex items-center text-xl justify-center">No data</div>
 {/if}
