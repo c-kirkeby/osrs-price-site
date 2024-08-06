12a4939a2c862134500aec941b9e1e1476e7ee94
c-kirkeby <christian@christiankirkeby.com>
2024-08-06 20:17:23 +1000

    chore: update shadcn-svelte from upstream

--- src/lib/components/ui/table/table-row.svelte
+++ src/lib/components/ui/table/table-row.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { cn } from "$lib/utils";
 	import type { HTMLAttributes } from "svelte/elements";
+	import { cn } from "$lib/utils.js";
 
 	type $$Props = HTMLAttributes<HTMLTableRowElement> & {
 		"data-state"?: unknown;
@@ -12,10 +12,12 @@
 
 <tr
 	class={cn(
-		"border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+		"hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",
 		className
 	)}
 	{...$$restProps}
+	on:click
+	on:keydown
 >
 	<slot />
 </tr>
