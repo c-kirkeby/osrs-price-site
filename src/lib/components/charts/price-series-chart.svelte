4fa4b8f2216c80becbb67989718ba72842c3c635
c-kirkeby <christian@christiankirkeby.com>
2024-08-17 18:57:37 +1000

    feat: copy shadcn-ui styles

--- src/lib/components/charts/price-series-chart.svelte
+++ src/lib/components/charts/price-series-chart.svelte
@@ -1,23 +1,19 @@
 <script lang="ts">
   import type { TimeSeries } from "$lib/types/time-series";
-  import { getCompactNumberFormatter, getNumberFormatter } from "$lib/utils";
-  import {
-    Chart,
-    Svg,
-    Axis,
-    Spline,
-    Tooltip,
-    TooltipItem,
-    Highlight,
-  } from "layerchart";
-  import { formatDistanceToNowStrict } from "date-fns";
-  import { scaleTime } from "d3-scale";
-  import { format } from "date-fns/format";
+  import { Chart, Svg, Axis, Spline, Tooltip, Highlight } from "layerchart";
+  import { formatDistanceToNowStrict, formatRelative } from "date-fns";
+  import { scaleOrdinal, scaleTime } from "d3-scale";
+  import capitalize from "lodash/capitalize";
+  import { getCompactNumberFormatter } from "$lib/utils";
 
-  $: numberFormatter = getNumberFormatter();
   $: compactNumberFormatter = getCompactNumberFormatter();
 
   export let data: TimeSeries[];
+
+  const chartColours = {
+    highPrice: "hsl(var(--chart-1))",
+    lowPrice: "hsl(var(--chart-5))",
+  };
 
   $: avgHighPriceSeries = data.filter((d) => d.avgHighPrice !== null);
   $: avgLowPriceSeries = data.filter((d) => d.avgLowPrice !== null);
@@ -46,15 +42,17 @@
 
 {#if data.length > 0}
   <div class="pb-4">
-    <div class="h-[200px]">
+    <div class="h-[250px]">
       <Chart
         {data}
         x="timestamp"
         y="avgHighPrice"
-        y1="avgLowPrice"
         xScale={scaleTime()}
         {yDomain}
         yNice
+        rDomain={Object.keys(chartColours)}
+        rRange={Object.values(chartColours)}
+        rScale={scaleOrdinal()}
         padding={{
           top: 5,
           right: 10,
@@ -64,44 +62,93 @@
         tooltip={{
           mode: "voronoi",
         }}
+        let:rScale
       >
         <Svg>
           <Axis
             placement="left"
             format={formatPrice}
-            rule={{ class: "stroke-primary" }}
+            rule={{ class: "hsl(var(--chart-1))" }}
             labelProps={{
-              class: "fill-primary",
+              class: "fill-foreground",
             }}
+            grid={{ class: "stroke-border/50" }}
           />
           <Axis
             placement="bottom"
             format={formatTimestamp}
-            rule={{ class: "stroke-primary" }}
             labelProps={{
-              class: "fill-primary",
+              class: "fill-foreground",
             }}
           />
           <Spline
             data={avgHighPriceSeries}
             y={avgHighPriceAccessor}
-            class="stroke-2 stroke-primary"
+            class="stroke-2 stroke-[hsl(var(--chart-1))]"
           />
           <Spline
             data={avgLowPriceSeries}
             y={avgLowPriceAccessor}
-            class="stroke-2 stroke-secondary"
+            class="stroke-2 stroke-[hsl(var(--chart-5))]"
           />
-          <Highlight points lines />
+          <Highlight
+            points={{
+              tweened: false,
+              spring: false,
+              r: 0.5,
+            }}
+          ></Highlight>
         </Svg>
         <Tooltip
-          header={(d) =>
-            format(new Date(d.timestamp * 1000), "yyyy-MM-dd HH:mm:ss")}
           let:data
+          classes={{
+            container:
+              "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-background px-2.5 text-xs shadow-xl",
+            content: "grid gap-1.5",
+          }}
         >
-          <TooltipItem label="High Price" value={data.avgHighPrice} />
-          <TooltipItem label="Low Price" value={data.avgLowPrice} />
-        </Tooltip>
+          <div slot="header" let:data>
+            {capitalize(
+              formatRelative(data.timestamp * 1000, new Date()).slice(),
+            )}
+          </div>
+          <div class="grid gap-1.5">
+            {#if data.avgHighPrice}
+              <div
+                class="flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 [&>svg]:text-muted-foreground"
+              >
+                <div
+                  class="shrink-0 rounded-[2px] border-[hsl(var(--chart-1))] bg-[hsl(var(--chart-1))] w-2.5 h-2.5"
+                ></div>
+                <div class="flex flex-1 justify-between leading-none">
+                  <span class="text-muted-foreground">High Price</span>
+                </div>
+                <span
+                  class="font-mono font-medium tabular-nums text-foreground"
+                >
+                  {data.avgHighPrice.toLocaleString()}
+                </span>
+              </div>
+            {/if}
+            {#if data.avgLowPrice}
+              <div
+                class="flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 [&>svg]:text-muted-foreground"
+              >
+                <div
+                  class="shrink-0 rounded-[2px] border-[hsl(var(--chart-5))] bg-[hsl(var(--chart-5))] w-2.5 h-2.5"
+                ></div>
+                <div class="flex flex-1 justify-between leading-none">
+                  <span class="text-muted-foreground">Low Price</span>
+                </div>
+                <span
+                  class="font-mono font-medium tabular-nums text-foreground"
+                >
+                  {data.avgLowPrice.toLocaleString()}
+                </span>
+              </div>
+            {/if}
+          </div></Tooltip
+        >
       </Chart>
     </div>
   </div>
--- src/routes/items/[id]/(components)/time-step-dropdown.svelte
+++ src/routes/items/[id]/(components)/time-step-dropdown.svelte
@@ -2,20 +2,22 @@
   import * as Select from "$lib/components/ui/select";
 
   const timeSteps = [
-    { value: "5m", label: "1 Day" },
-    { value: "1h", label: "7 Days" },
-    { value: "6h", label: "1 Month" },
-    { value: "24h", label: "1 Year" },
+    { value: "5m", label: "Last day" },
+    { value: "1h", label: "Last 7 days" },
+    { value: "6h", label: "Last 30 days" },
+    { value: "24h", label: "Last 12 months" },
   ];
 </script>
 
 <Select.Root {...$$restProps}>
-  <Select.Trigger class="w-[180px]">
+  <Select.Trigger
+    class="w-[160px] rounded-lg sm:ml-auto"
+    aria-label="Select an interval"
+  >
     <Select.Value placeholder="Select an interval" />
   </Select.Trigger>
-  <Select.Content>
+  <Select.Content class="rounded-xl">
     <Select.Group>
-      <Select.Label>Interval</Select.Label>
       {#each timeSteps as { value, label }}
         <Select.Item {value} {label}>{label}</Select.Item>
       {/each}
