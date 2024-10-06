a00c69264405909726bab2810f4fab1b65551450
c-kirkeby <christian@christiankirkeby.com>
2024-10-06 15:05:13 +1000

    chore: remove unused recipes files

--- src/routes/recipes/(components)/recipe-item-cell.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { DataTableLink } from "$lib/components/data-table";
-  import DataTableImage from "$lib/components/data-table/data-table-image.svelte";
-
-  export let value;
-</script>
-
-<DataTableImage {src} />
-<DataTableLink {value} {...$$restProps}></DataTableLink>
