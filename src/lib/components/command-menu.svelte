11b7ba8bf48c8f5b091daa1b6ba6b094f3952d69
Christian <christian@christiankirkeby.com>
2026-08-15 20:50:34 +1000

    fix: command menu after svelte 5 upgrade

--- src/lib/components/command-menu.svelte
+++ src/lib/components/command-menu.svelte
@@ -1,21 +1,18 @@
 <script lang="ts">
-  import { run } from 'svelte/legacy';
-
   import { Button } from "$lib/components/ui/button";
   import * as Command from "$lib/components/ui/command";
   import type { Item } from "$lib/types/item";
   import { searchHistory } from "$lib/stores/search-history";
   import { getUserOperatingSystem } from "$lib/utils";
-  import { onMount } from "svelte";
   import { goto } from "$app/navigation";
   import { browser } from "$app/environment";
-  import { X, Sun, Moon, Laptop } from "lucide-svelte";
+  import { X, Sun, Moon, Laptop, XIcon } from "lucide-svelte";
   import { resetMode, setMode } from "mode-watcher";
   import { createItemsIndex, searchItemsIndex } from "$lib/search";
   import { itemsStore } from "$lib/stores/items";
-  interface Props {
-    [key: string]: any
-  }
+  import * as Kbd from "$lib/components/ui/kbd";
+  import * as Dialog from "$lib/components/ui/dialog";
+  type Props = Record<string, any>;
 
   let { ...rest }: Props = $props();
 
@@ -25,14 +22,14 @@
   let status: "loading" | "ready" = $state("loading");
   const platform = browser && getUserOperatingSystem();
 
-  run(() => {
+  $effect(() => {
     if ($itemsStore?.length && $itemsStore.length > 0) {
       createItemsIndex($itemsStore);
       status = "ready";
     }
   });
 
-  run(() => {
+  $effect(() => {
     if (status === "ready") {
       const search = async () => {
         results = await searchItemsIndex(value);
@@ -46,121 +43,126 @@
     command();
   }
 
-  onMount(() => {
-    function handleKeyDown(event: KeyboardEvent) {
-      if (event.key === "k" && (event.metaKey || event.ctrlKey)) {
-        event.preventDefault();
-        open = !open;
-      }
+  function handleKeyDown(event: KeyboardEvent) {
+    if (event.key === "k" && (event.metaKey || event.ctrlKey)) {
+      event.preventDefault();
+      open = !open;
     }
-    document.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  });
+  }
+  const shortcutModifier = $derived.by(() =>
+    platform === "MacOS" ? "⌘" : "Ctrl",
+  );
 </script>
 
-<Button
-  variant="outline"
-  class="relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64"
-  on:click={() => (open = true)}
-  {...rest}
->
-  <span class="hidden lg:inline-flex"> Search items </span>
-  <span class="inline-flex lg:hidden">Search</span>
-  <kbd
-    class="pointer-events-none absolute right-2 top-2 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-2 font-mono text-[10px] font-medium opacity-100 sm:flex"
-  >
-    <span class="text-xs"
-      >{platform === "MacOS" ? "⌘" : "Ctrl"} <span>K</span></span
-    >
-  </kbd>
-</Button>
-<Command.Dialog bind:open shouldFilter={false}>
-  <Command.Input placeholder="Type a command or search" bind:value />
-  <Command.List>
-    <Command.Empty>No results found.</Command.Empty>
-    {#if results.length > 0}
-      <Command.Group heading="Items">
-        {#each results.slice(0, 5) as result}
-          <Command.Item
-            onSelect={() =>
-              runCommand(() => {
-                open = false;
-                searchHistory.add(result);
-                goto(`/items/${result.id}`);
-              })}
-          >
-            <img
-              src={`https://oldschool.runescape.wiki/images/${encodeURIComponent(
-                result.icon?.replaceAll(" ", "_") ?? "",
-              )}`}
-              alt={result.name}
-              class="object-contain inline-block mr-2 h-4 w-4"
-            />
-            {result.name}
-          </Command.Item>
-        {/each}
-      </Command.Group>
-      <Command.Separator />
-    {/if}
-    {#if $searchHistory.length > 0}
-      <Command.Group heading="History">
-        {#each $searchHistory as item}
-          <Command.Item
-            onSelect={() =>
-              runCommand(() => {
-                open = false;
-                goto(`/items/${item.id}`);
-              })}
-          >
-            <img
-              src={`https://oldschool.runescape.wiki/images/${encodeURIComponent(
-                item.icon?.replaceAll(" ", "_") ?? "",
-              )}`}
-              alt={item.name}
-              class="object-contain inline-block mr-2 h-4 w-4"
-            />
-            {item.name}
-            <Button
-              variant="ghost"
-              size="sm"
-              class="ml-auto h-5"
-              on:click={(event) => {
-                event.stopPropagation();
-                searchHistory.remove(item);
-              }}
+<svelte:document onkeydown={handleKeyDown} />
+
+<Dialog.Root bind:open>
+  <Dialog.Trigger>
+    {#snippet child({ props })}
+      <Button
+        {...props}
+        variant="outline"
+        class="relative w-full justify-between text-sm text-muted-foreground md:w-40 lg:w-64"
+        onclick={() => (open = true)}
+      >
+        <span class="hidden lg:inline-flex"> Search items </span>
+        <span class="inline-flex lg:hidden">Search</span>
+        <Kbd.Group>
+          <Kbd.Root>
+            {shortcutModifier}
+          </Kbd.Root>
+          <Kbd.Root>K</Kbd.Root>
+        </Kbd.Group>
+      </Button>
+    {/snippet}
+  </Dialog.Trigger>
+  <Command.Dialog bind:open shouldFilter={false}>
+    <Command.Input placeholder="Type a command or search" bind:value />
+    <Command.List>
+      <Command.Empty>No results found.</Command.Empty>
+
+      {#if results.length > 0}
+        <Command.Group heading="Items">
+          {#each results.slice(0, 5) as result}
+            <Command.Item
+              onSelect={() =>
+                runCommand(() => {
+                  open = false;
+                  searchHistory.add(result);
+                  goto(`/items/${result.id}`);
+                })}
             >
-              <X class="h-0.5" />
-            </Button>
-          </Command.Item>
-        {/each}
+              <img
+                src={`https://oldschool.runescape.wiki/images/${encodeURIComponent(
+                  result.icon?.replaceAll(" ", "_") ?? "",
+                )}`}
+                alt={result.name}
+                class="object-contain inline-block mr-2 h-4 w-4"
+              />
+              {result.name}
+            </Command.Item>
+          {/each}
+        </Command.Group>
+        <Command.Separator />
+      {/if}
+
+      {#if $searchHistory.length > 0}
+        <Command.Group heading="History">
+          {#each $searchHistory as item}
+            <Command.Item
+              onSelect={() =>
+                runCommand(() => {
+                  open = false;
+                  goto(`/items/${item.id}`);
+                })}
+            >
+              <img
+                src={`https://oldschool.runescape.wiki/images/${encodeURIComponent(
+                  item.icon?.replaceAll(" ", "_") ?? "",
+                )}`}
+                alt={item.name}
+                class="object-contain block h-4 w-4"
+              />
+              <span>{item.name}</span>
+              <Button
+                variant="ghost"
+                size="sm"
+                class="ml-auto h-5"
+                onclick={(event) => {
+                  event.stopPropagation();
+                  searchHistory.remove(item);
+                }}
+              >
+                <X class="h-3 w-3" />
+              </Button>
+            </Command.Item>
+          {/each}
+        </Command.Group>
+        <Command.Separator />
+      {/if}
+      <Command.Group heading="Theme">
+        <Command.Item
+          value="light"
+          onSelect={() => runCommand(() => setMode("light"))}
+        >
+          <Sun class="mr-2 h-4 w-4" />
+          Light
+        </Command.Item>
+        <Command.Item
+          value="dark"
+          onSelect={() => runCommand(() => setMode("dark"))}
+        >
+          <Moon class="mr-2 h-4 w-4" />
+          Dark
+        </Command.Item>
+        <Command.Item
+          value="system"
+          onSelect={() => runCommand(() => resetMode())}
+        >
+          <Laptop class="mr-2 h-4 w-4" />
+          System
+        </Command.Item>
       </Command.Group>
-      <Command.Separator />
-    {/if}
-    <Command.Group heading="Theme">
-      <Command.Item
-        value="light"
-        onSelect={() => runCommand(() => setMode("light"))}
-      >
-        <Sun class="mr-2 h-4 w-4" />
-        Light
-      </Command.Item>
-      <Command.Item
-        value="dark"
-        onSelect={() => runCommand(() => setMode("dark"))}
-      >
-        <Moon class="mr-2 h-4 w-4" />
-        Dark
-      </Command.Item>
-      <Command.Item
-        value="system"
-        onSelect={() => runCommand(() => resetMode())}
-      >
-        <Laptop class="mr-2 h-4 w-4" />
-        System
-      </Command.Item>
-    </Command.Group>
-  </Command.List>
-</Command.Dialog>
+    </Command.List>
+  </Command.Dialog>
+</Dialog.Root>
