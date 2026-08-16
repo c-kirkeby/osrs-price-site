7346723692b770490b21edf33ce42714d1db6a3e
Christian <christian@christiankirkeby.com>
2026-08-16 10:30:11 +1000

    fix: update select from shadcn-svelte

--- src/lib/components/ui/select/select.svelte
+++ src/lib/components/ui/select/select.svelte
@@ -1,12 +1,11 @@
-<!-- @migration-task Error while migrating Svelte code: Can only bind to an Identifier or MemberExpression or a `{get, set}` pair
-https://svelte.dev/e/bind_invalid_expression -->
 <script lang="ts">
-  import { Select as SelectPrimitive } from "bits-ui";
-  let {
-    open = $bindable(false),
-    value = $bindable(),
-    ...restProps
-  }: SelectPrimitive.RootProps = $props();
+	import { Select as SelectPrimitive } from "bits-ui";
+
+	let {
+		open = $bindable(false),
+		value = $bindable(),
+		...restProps
+	}: SelectPrimitive.RootProps = $props();
 </script>
 
 <SelectPrimitive.Root bind:open bind:value={value as never} {...restProps} />
